0a0078d633b63f0555905c1e7bf2a587e98b39db
Krishay05 <120331668+Krishay05@users.noreply.github.com>
2022-12-11 13:09:17 -0500

    eee
    aaa

---
+++ jdoodle.lua
@@ -0,0 +1,9 @@
+If game.PlaceId == then
+    local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+    local Window = OrionLib:MakeWindow((Name = "Tutorial Hub", HidePremium = false, SaveConfig = true, config folder = "tutorialConfig"))
+    
+    
+    
+    
+    end
+OrionLib:Init()
